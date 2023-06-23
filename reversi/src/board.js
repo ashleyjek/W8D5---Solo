e2a9b9c6b7b368530b8a774b85c81a0e6a247e59
ashley <ashleykimca@gmail.com>
2023-06-23 15:57:20 -0400

    reversi

---
+++ reversi/src/board.js
@@ -0,0 +1,188 @@
+// DON'T TOUCH THIS CODE
+if (typeof window === 'undefined'){
+  const Piece = require("./piece");
+}
+// DON'T TOUCH THIS CODE
+
+
+/**
+ * Returns a 2D array (8 by 8) with two black pieces at [3, 4] and [4, 3]
+ * and two white pieces at [3, 3] and [4, 4].
+ */
+function _makeGrid() {
+  let grid = [];
+  const x = 8;
+
+  for (let i = 0; i < x; i++) {
+    grid[i] = new Array(8);
+  }
+  
+  grid[3][4] = new Piece('black');
+  grid[4][3] = new Piece('black');
+  grid[3][3] = new Piece('white');
+  grid[4][4] = new Piece('white');
+  return grid;
+}
+
+// console.log(_makeGrid());
+
+/**
+ * Constructs a Board with a starting grid set up.
+ */
+function Board () {
+  this.grid = _makeGrid();
+}
+
+
+Board.DIRS = [
+  [ 0,  1], [ 1,  1], [ 1,  0],
+  [ 1, -1], [ 0, -1], [-1, -1],
+  [-1,  0], [-1,  1]
+];
+
+/**
+ * Checks if a given position is on the Board.
+ */
+Board.prototype.isValidPos = function (pos) {
+  const x = pos[0];
+  const y = pos[1];
+  console.log(pos)
+  if (x < 0 || y < 0) {
+    return false;
+  } else if (x > 8 || y > 8) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * Returns the piece at a given [x, y] position,
+ * throwing an Error if the position is invalid.
+ */
+Board.prototype.getPiece = function (pos) {
+  const x = pos[0];
+  const y = pos[1];
+  if (this.isValidPos(pos)) {
+    return this.grid[x][y];
+  } else {
+    throw new Error('Not valid pos!');
+  }
+};
+
+/**
+ * Checks if the piece at a given position
+ * matches a given color.
+ */
+Board.prototype.isMine = function (pos, color) {
+  const x = pos[0];
+  const y = pos[1];
+  // if (!this.grid[x][y] || this.grid[x][y].color !== color) {
+  //   return false;
+  // } else { 
+  //   return true;
+  // }
+  if (this.grid[x][y]?.color === color) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * Checks if a given position has a piece on it.
+ */
+Board.prototype.isOccupied = function (pos) {
+  // const x = pos[0];
+  // const y = pos[1];
+  const [x, y] = pos;
+  if (this.grid[x][y]) {
+    return true;
+  } else {
+    return false;
+  }
+  // !!this.grid[x][y];
+};
+
+/**
+ * Recursively follows a direction away from a starting position, adding each
+ * piece of the opposite color until hitting another piece of the current color.
+ * It then returns an array of all pieces between the starting position and
+ * ending position.
+ *
+ * Returns an empty array if it reaches the end of the board before finding 
+ * another piece of the same color.
+ *
+ * Returns empty array if it hits an empty position.
+ *
+ * Returns empty array if no pieces of the opposite color are found.
+ */
+Board.prototype._positionsToFlip = function (pos, color, dir, piecesToFlip) {
+  let [x, y] = pos;
+  const [i, j] = dir;
+  const newPos = [x+i, y+j]
+  if (!this.isValidPos(pos)) {
+    return [];
+  } else if (this.isValidPos(newPos) && !this.isOccupied(newPos)) {
+    return [];
+  } else if (this.grid.color !== color) {
+    return [];
+  }
+
+  
+
+}
+
+
+/**
+ * Checks that a position is not already occupied and that the color
+ * taking the position will result in some pieces of the opposite
+ * color being flipped.
+ */
+Board.prototype.validMove = function (pos, color) {
+  const x = pos[0];
+  const y = pos[1];
+  // if (!this.isOccupied(pos)) {
+
+  // }
+};
+
+/**
+ * Adds a new piece of the given color to the given position, flipping the
+ * color of any pieces that are eligible for flipping.
+ *
+ * Throws an error if the position represents an invalid move.
+ */
+Board.prototype.placePiece = function (pos, color) {
+};
+
+/**
+ * Produces an array of all valid positions on
+ * the Board for a given color.
+ */
+Board.prototype.validMoves = function (color) {
+};
+
+/**
+ * Checks if there are any valid moves for the given color.
+ */
+Board.prototype.hasMove = function (color) {
+};
+
+/**
+ * Checks if both the white player and
+ * the black player are out of moves.
+ */
+Board.prototype.isOver = function () {
+};
+
+/**
+ * Prints a string representation of the Board to the console.
+ */
+Board.prototype.print = function () {
+};
+
+// DON'T TOUCH THIS CODE
+if (typeof window === 'undefined'){
+  module.exports = Board;
+}
+// DON'T TOUCH THIS CODE
